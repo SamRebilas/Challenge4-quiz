9415b42bbb31b69c9e1ba7e83c8b75fedf3e17c0
Samrebilas <sam.rebilas@gmail.com>
2022-06-29 16:48:03 -0400

    buttons now display possible answers for first question

--- script.js
+++ script.js
@@ -2,8 +2,13 @@ var startButton = document.getElementById('start-quiz')
 var ansButton = document.getElementById('answers')
 var intro = document.getElementById('introduction')
 var questionElement = document.getElementById('question')
-var choicesElement = document.getElementById("button1")
-
+//var choicesElement = document.getElementById('1')
+var currentQuestion = 0
+var currentChoices = 0
+var buttonA = document.getElementById('a')
+var buttonB = document.getElementById('b')
+var buttonC = document.getElementById('c')
+var buttonD = document.getElementById('d')
 
 
 function beginQuiz(){
@@ -11,17 +16,19 @@ function beginQuiz(){
     startButton.style.display = "none";
     ansButton.style.display = "";
     intro.style.display = "none"
-   //question.style.display = [questions]
-    currentQuestion = 0
+   
     nextQuestion()
     
 }
 // quiz questions
 var questions = [
     {
-        question: "Commonly used data types do NOT include:",
-        correctAnswer: 1,
-        choices: ["strings", "booleans", "alerts", "numbers"],
+        question: "1. Commonly used data types do NOT include:",
+        correctAnswer: buttonB,
+        choiceA: "strings", 
+        choiceB: "booleans", 
+        choiceC: "alerts", 
+        choiceD: "numbers",
     },
     {
         question: "The condition in an if / else statement is enclosed with _____.",
@@ -32,11 +39,30 @@ var questions = [
 
 function nextQuestion(){
    displayQuestion(questions[currentQuestion])
+   
+  
+
+   
 }
-function displayQuestion(question) {
-questionElement.innerText = question.question
-choicesElement.innerText = question.choices
+function displayQuestion(questions) {
+questionElement.innerText = questions.question
+
+buttonA.innerHTML = questions.choiceA;
+buttonB.innerHTML = questions.choiceB;
+buttonC.innerHTML = questions.choiceC;
+buttonD.innerHTML = questions.choiceD;
+if (buttonB.clicked)
+console.log(buttonB)
+
 }
 
-startButton.addEventListener('click', beginQuiz, ansButton, intro )
+
+
+for(var i = 0; i < questions.length; i++) {
+    var questionName = questions[currentQuestion].question
+    var choicesName = questions[currentQuestion].choices
+    console.log(i)
+}
+
+startButton.addEventListener('click', beginQuiz, ansButton, intro, )
 

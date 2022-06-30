c0f1fe0c7ab1dd5f474a0dd1a006b348e54621a3
Samrebilas <sam.rebilas@gmail.com>
2022-06-30 16:21:00 -0400

    timer is working and subtracting correctly

--- script.js
+++ script.js
@@ -2,14 +2,14 @@ var startButton = document.getElementById('start-quiz')
 var ansButton = document.getElementById('answers')
 var intro = document.getElementById('introduction')
 var questionElement = document.getElementById('question')
-//var choicesElement = document.getElementById('1')
+var timer = document.getElementById("timer")
 var currentQuestion = 0
 var currentChoices = 0
 var buttonA = document.getElementById('a')
 var buttonB = document.getElementById('b')
 var buttonC = document.getElementById('c')
 var buttonD = document.getElementById('d')
-
+var timeRemaining = 60;
 
 function beginQuiz(){
     console.log('started')
@@ -18,6 +18,7 @@ function beginQuiz(){
     intro.style.display = "none"
    
     firstQuestion()
+    countdownTimer()
     
 }
 // quiz questions
@@ -25,77 +26,192 @@ var questionBank = [
     {
         question: "1. Commonly used data types do NOT include:",
         choices:[
-        {text: "strings", correctAnswer: false} ,
-        {text: "booleans", correctAnswer: true },
-        {text: "alerts", correctAnswer: false},
-        {text: "numbers", correctAnswer: false},
+        {text: "1. strings", correctAnswer: false},
+        {text: "2. booleans", correctAnswer: true },
+        {text: "3. alerts", correctAnswer: false},
+        {text: "4. numbers", correctAnswer: false},
         ],
     },
     {
         question: "2. The condition in an if / else statement is enclosed with ______.",
         choices:[
-        {text: "quotes", correctAnswer: false} ,
-        {text: "curly brackets", correctAnswer: false },
-        {text: "parenthesis", correctAnswer: true},
-        {text: "square brackets", correctAnswer: false},
+        {text: "1. quotes", correctAnswer: false},
+        {text: "2. curly brackets", correctAnswer: false },
+        {text: "3. parenthesis", correctAnswer: true},
+        {text: "4. square brackets", correctAnswer: false},
         ],
     },
+    {
+        question: "3. Arrays in javaScript can be used to store ______.",
+        choices:[
+        {text: "1. numbers and strings", correctAnswer: false},
+        {text: "2. other arrays", correctAnswer: false },
+        {text: "3. booleans", correctAnswer: true},
+        {text: "4. all of the above", correctAnswer: false},
+        ],
+    },
+    {
+        question: "4. string values must be enclosed within ____ when being assigned to variables.",
+        choices:[
+        {text: "1. commas", correctAnswer: false},
+        {text: "2. curly brackets", correctAnswer: false},
+        {text: "3. quotes", correctAnswer: true},
+        {text: "4. parenthesis", correctAnswer: false},
+        ],
+    },
+    {
+        question: "5. A very useful tool used during development and debugging for printing content to the debugger is",
+        choices:[
+        {text: "1. javaScript", correctAnswer: false} ,
+        {text: "2. terminal/bash", correctAnswer: false },
+        {text: "3. console.log", correctAnswer: true},
+        {text: "4. for loops", correctAnswer: false},
+        ],
+    },
+   
 ];
-
-function firstQuestion(){
-displayQuestion(questionBank[currentQuestion])
-   buttonA.addEventListener("click", chooseAnswer)
-   buttonB.addEventListener("click", chooseAnswer)
-   buttonC.addEventListener("click", chooseAnswer)
-   buttonD.addEventListener("click", chooseAnswer)
-
- // questionBank.question++
-  
+function displayQuestion(questionBank) {
+    questionElement.innerText = questionBank.question
+    
+    buttonA.innerHTML = questionBank.choices[0].text;
+    buttonA.setAttribute("choices", questionBank.choices[0].correctAnswer)
+    
+    buttonB.innerHTML = questionBank.choices[1].text;
+    buttonB.setAttribute("choices", questionBank.choices[1].correctAnswer)
+    
+    buttonC.innerHTML = questionBank.choices[2].text;
+    buttonC.setAttribute("choices", questionBank.choices[2].correctAnswer)
+    
+    buttonD.innerHTML = questionBank.choices[3].text;
+    buttonD.setAttribute("choices", questionBank.choices[3].correctAnswer)
+    
+ 
 
    
-}
-function displayQuestion(questionBank) {
-questionElement.innerText = questionBank.question
-
-buttonA.innerHTML = questionBank.choices[0].text;
-buttonA.setAttribute("choices", questionBank.choices[0].correctAnswer)
-
-buttonB.innerHTML = questionBank.choices[1].text;
-buttonB.setAttribute("choices", questionBank.choices[1].correctAnswer)
-
-buttonC.innerHTML = questionBank.choices[2].text;
-buttonC.setAttribute("choices", questionBank.choices[2].correctAnswer)
-
-buttonD.innerHTML = questionBank.choices[3].text;
-buttonD.setAttribute("choices", questionBank.choices[3].correctAnswer)
-
-//questionBank.question++;
-}
-
-function chooseAnswer(event) {
-    var chooseAnswer = event.target.getAttribute("choices");
-    if (chooseAnswer === "true") {
-        window.alert("Correct!")
-        secondQuestion()
+    
     }
-    else {
-        window.alert("Sorry that is incorrect.")
+
+function firstQuestion(){
+    displayQuestion(questionBank[0])
+
+    buttonA.addEventListener("click", chooseAnswer)
+    buttonB.addEventListener("click", chooseAnswer)
+    buttonC.addEventListener("click", chooseAnswer)
+    buttonD.addEventListener("click", chooseAnswer)
+
+    function chooseAnswer(event) {
+        var chooseAnswer = event.target.getAttribute("choices");
+        if (chooseAnswer === "true") {
+            window.alert("Correct!")
+            secondQuestion()
+            
+        }
+        else {
+            window.alert("Sorry that is incorrect.")
+            timeRemaining = timeRemaining - 10
+        }
     }
 }
-
 function secondQuestion(){
-  displayQuestion(questionBank[1])
+    displayQuestion(questionBank[1])
+    buttonA.addEventListener("click", chooseAnswer)
+    buttonB.addEventListener("click", chooseAnswer)
+    buttonC.addEventListener("click", chooseAnswer)
+    buttonD.addEventListener("click", chooseAnswer)
+
+    function chooseAnswer(event) {
+        var chooseAnswer = event.target.getAttribute("choices");
+        if (chooseAnswer === "true") {
+            
+            thirdQuestion()
+            
+    }
+    else{
+        
+    }
+    }
+}
+function thirdQuestion(){
+    displayQuestion(questionBank[2])
+    buttonA.addEventListener("click", chooseAnswer)
+    buttonB.addEventListener("click", chooseAnswer)
+    buttonC.addEventListener("click", chooseAnswer)
+    buttonD.addEventListener("click", chooseAnswer)
+    
+    function chooseAnswer(event) {
+        var chooseAnswer = event.target.getAttribute("choices");
+        if (chooseAnswer === "true") {
+           
+            fourthQuestion()
+            
+        }
+        else{
+            
+        }
+        
+    }
+}
+    
+function fourthQuestion(){
+    displayQuestion(questionBank[3])
+    buttonA.addEventListener("click", chooseAnswer)
+    buttonB.addEventListener("click", chooseAnswer)
+    buttonC.addEventListener("click", chooseAnswer)
+    buttonD.addEventListener("click", chooseAnswer)
+    
+    function chooseAnswer(event) {
+        var chooseAnswer = event.target.getAttribute("choices");
+        if (chooseAnswer === "true") {
+           
+            fifthQuestion()
+            
+        }
+        else{
+            
+        }
+        
+    }
+}
+
+function fifthQuestion(){
+    displayQuestion(questionBank[4])
+    buttonA.addEventListener("click", chooseAnswer)
+    buttonB.addEventListener("click", chooseAnswer)
+    buttonC.addEventListener("click", chooseAnswer)
+    buttonD.addEventListener("click", chooseAnswer)
+
+    function chooseAnswer(event) {
+             var chooseAnswer = event.target.getAttribute("choices");
+            if (chooseAnswer === "true"){
+                finalScore()
+                }
+                else{
+                    
+                }
+    }
+ }
+
+ function countdownTimer(){
+ var timeInterval = setInterval(function(){
+    timer.textContent = "Time: " + timeRemaining
+    timeRemaining --
+    
+if (timeRemaining < 10){
+timer.textContent= "10 seconds remaining!  " + timeRemaining
+timeRemaining --
+   
+}
+
+ 
+},1000)
 
 }
 
+function finalScore(){
+    window.alert("Congrats! your score is " + timeRemaining)
+}
 
 
-
-//for(var i = 0; i < questionBank.length; i++) {
-   // var questionName = questionBank[currentQuestion].question
-   // var choicesName = questionBank[currentQuestion].choices
-   // console.log(i)
-//}
 
 startButton.addEventListener('click', beginQuiz, ansButton, intro, )
 
